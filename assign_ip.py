a390dba5dc5343e34d265aa8cbe7566a03fd3617
Brian Sherwood <bsherwood@juniper.net>
2023-03-13 08:46:38 -0400

    WIP

--- assign_ip.py
+++ assign_ip.py
@@ -10,43 +10,33 @@ with open("topology.yaml", "r") as stream:
     except yaml.YAMLError as exc:
         print(exc)
 
-print ("topo = ")
-pprint (topo_data)
+#print ("topo = ")
+#pprint (topo_data)
+
+def get_edge_list(topo_data):
+    edge_list = []
+    for device, ports in topo_data['topo'].items():
+        #print ("\n")
+        #print ("device = ",device)
+        for port_num,port_data in ports.items():
+            #print ("\n")
+            #print ("   ","port_num = ",port_num)
+            #print ("   ","port_data = ",port_data)
+            #print ("   ","port_data[name] = ",port_data['name'])
+            #print ("   ","port_data[peer] = ",port_data['peer'])
+            #print ("   ","port_data[pport] = ",port_data['pport'])
+            #print ("   ","peer = ",port_data["peer"])
+            #print ("   ","peer port = ",topo_data['topo'][port_data["peer"]][port_data['pport']]['name'])
+            peer_device = port_data["peer"]
+            peer_port = topo_data['topo'][port_data["peer"]][port_data['pport']]['name']
+            #print ("   ","(",device,",",port_data['name'],",",peer_device,",",peer_port,")")
+            #print ("   ","peer = ",topo_data['topo'][port_data["peer"]][port_data['pport']]['name'])
+            edge_list.append([device,port_data['name'],peer_device,peer_port])
+    return edge_list
 
 
-for device, ports in topo_data['topo'].items():
-     print ("\n")
-     print ("device = ",device)
-     #print ("ports = ",ports)
-     for port_num,port_data in ports.items():
-         print ("\n")
-         print ("   ","port_num = ",port_num)
-         print ("   ","port_data = ",port_data)
-         print ("   ","port_data[name] = ",port_data['name'])
-         print ("   ","port_data[peer] = ",port_data['peer'])
-         print ("   ","port_data[pport] = ",port_data['pport'])
-         print ("   ","peer = ",port_data["peer"])
-         print ("   ","peer port = ",topo_data['topo'][port_data["peer"]][port_data['pport']]['name'])
-         peer_device = port_data["peer"]
-         peer_port = topo_data['topo'][port_data["peer"]][port_data['pport']]['name']
-         print ("   ","(",device,",",port_data['name'],",",peer_device,",",peer_port,")")
-         #print ("   ","peer = ",topo_data['topo'][port_data["peer"]][port_data['pport']]['name'])
 
-
-
-
-#for device, ports in topo_data['topo'].items():
-#     print("\n",device, '->', ports)
-#     if not(re.search(r'^vMX', device)):
-#         print ("Skip ",device)
-#         continue
-#     for port in ports:
-#         print ("PORT = ",port)
-#         #print ("   ",ports[port])
-#         if not(re.search(r'^vMX',ports[port]["peer"])):
-#             print ("Skip peer ",ports[port]["peer"])
-#             continue
-#         print ("Process ",ports[port])
-
-
+if __name__ == "__main__":
+    edge_list = get_edge_list(topo_data)
+    pprint (edge_list)
 
